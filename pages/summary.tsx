6a3c0817d32467a8e4f5f3bfabb582007f681b84
Diego Mernies <diego.mernies@securitize.io>
2023-08-07 00:40:55 -0300

    feat: added new screen to display summary

---
+++ pages/summary.tsx
@@ -0,0 +1,19 @@
+import {Box, Paper, TextField} from "@mui/material";
+import LagSummaryTable from "../src/LagSummaryTable";
+
+export default function Summary() {
+    return (
+        <Paper elevation={3} sx={{p: 12, pt: 1}} >
+            <Box sx={{ p: 2 }} textAlign={'center'} >
+                <h1> Cotización: Análisis de Laboratorios </h1>
+                <TextField id="full-name"
+                           label="Nombre completo del paciente"
+                           variant="outlined"
+                           required={true}
+                           fullWidth
+                           />
+                <LagSummaryTable />
+            </Box>
+        </Paper>
+    );
+}
