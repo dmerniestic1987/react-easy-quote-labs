0a3d551133ae526d8c8d0096e6778fb2de6da7f1
Diego Mernies <diego.mernies@securitize.io>
2023-08-13 23:13:18 -0300

    feat: adding total quoute

--- src/TotalQuote.tsx
+++ src/TotalQuote.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
-import {Paper} from "@mui/material";
+import {Alert, Paper} from "@mui/material";
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 
 export default function TotalQuote(props: any) {
   return (
       <Paper elevation={3} >
-          <Box>
+          <Alert sx={{ display: "flex", justifyContent: "center"}} severity="info" >
               <Typography variant="h5" component="h1" gutterBottom align="center">
                   {props.description}: $ {props.totalQuote}
               </Typography>
-          </Box>
+          </Alert>
       </Paper>
   );
 }
