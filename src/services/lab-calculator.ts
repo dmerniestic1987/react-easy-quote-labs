7b94a67333555199d210a53b51b0546c866e50ac
Diego Mernies <diego.mernies@securitize.io>
2024-08-01 11:08:04 -0300

    feat: update prices with latest prices 2024 agust

--- src/services/lab-calculator.ts
+++ src/services/lab-calculator.ts
@@ -1,5 +1,5 @@
 import BigNumber from 'bignumber.js';
-import labItemsData from '../../data/lab-aug-2024.json';
+import labItemsData from '../../data/lab-aug-2024-final.json';
 
 export const spanishAmountFmt = {
   prefix: '',
