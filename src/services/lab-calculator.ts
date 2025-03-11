79f6a64d43f1bc5c354f8ebcdaef1eb02e77faf4
Luis Zegarra <luis.zegarra@td8software.com>
2025-03-11 18:57:26 -0300

    update prices with latest prices 2025 march

--- src/services/lab-calculator.ts
+++ src/services/lab-calculator.ts
@@ -1,5 +1,5 @@
 import BigNumber from 'bignumber.js';
-import labItemsData from '../../data/lab-aug-2024-final.json';
+import labItemsData from '../../data/lab-march-2025.json';
 
 export const spanishAmountFmt = {
   prefix: '',
